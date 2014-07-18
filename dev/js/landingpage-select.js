d979b89b202c0fb4c3946d2d10aa5b3e4f1d6dce
Marten Sytema <marten@sytematic.nl>
2014-07-18 08:57:51 +0200

    few minor tweaks

--- dev/js/landingpage-select.js
+++ dev/js/landingpage-select.js
@@ -13,7 +13,7 @@ jQuery(document).ready(function($) {
 
     /* Clone our navigation(s) */
     var counter = 0;
-    $('#menu-footermenu').each(function() {
+    $('#menu-footermenu, #menu-menu').each(function() {
       var mainNavigation = $(this).clone();
 
       /* Replace unordered list with a "select" element to be populated with options,
